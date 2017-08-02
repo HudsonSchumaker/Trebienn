0956ef69e8567f1817780170c37d42613a49e964
Hudson Schumaker <hudson.schumaker@hotmail.com>
2017-08-01 22:24:48 -0300

    Frist Trebienn

---
+++ Assets/Scripts/HsBackground.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+/* 
+	@author Hudson Schumaker
+	@version 1.0.0
+*/
+public class HsBackground : MonoBehaviour {
+
+	private SpriteRenderer grafico;
+	private float larguraImagem;
+	private float alturaImagem;
+	private float alturaTela;
+	private float larguraTela;
+
+	private void Start () {
+		grafico = GetComponent<SpriteRenderer> ();
+
+		// Width and Heigth do sprite
+		larguraImagem = grafico.sprite.bounds.size.x;
+		alturaImagem = grafico.sprite.bounds.size.y;
+
+		// Width and Heigth da tela
+		alturaTela = Camera.main.orthographicSize * 2.0f;
+		larguraTela = alturaTela / Screen.height * Screen.width;
+
+		Vector2 novaEscala = transform.localScale;
+		novaEscala.x = larguraTela/larguraImagem; // + 0.25f para nao risco em background scroller 
+		novaEscala.y = alturaTela/alturaImagem;
+		this.transform.localScale = novaEscala;
+	}
+
+	private void Update () {
+	}
+}
